decd911150ae815e7ad48e8b621234913ee7c79b
Ponkarthick <ponkarthickramu16@gmail.com>
2026-01-08 21:30:51 +0530

    Unga message inge

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ def scaling_monitor():
     print("--- EduScale E-Learning Monitoring Started ---", flush=True) # flush=True add pannunga
     while True:
         cpu = psutil.cpu_percent(interval=1)
-        print(f"Current Traffic Load: {cpu}%", flush=True)
+        print(f"Current Traffic Loading : {cpu}%", flush=True)
         
         if cpu > 70:
             print(">>> ALERT: High Traffic! Scaling up...", flush=True)
